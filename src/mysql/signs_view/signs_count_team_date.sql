541ef46c718f7a51b81ef38d88bba988217ebb41
Romeo <1159328306@qq.com>
2021-01-24 20:44:43 +0800

    更改签到模块相关SQL脚本

--- src/mysql/signs_view/signs_count_team_date.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `signs_count_team_date` (`sign_date`, `team_id`, `team_name`, `sign_count`) VALUES ('2021-01-21', 1, 'BARTeam', 2);
-INSERT INTO `signs_count_team_date` (`sign_date`, `team_id`, `team_name`, `sign_count`) VALUES ('2021-01-22', 1, 'BARTeam', 1);
-INSERT INTO `signs_count_team_date` (`sign_date`, `team_id`, `team_name`, `sign_count`) VALUES ('2021-01-24', 1, 'BARTeam', 2);
-INSERT INTO `signs_count_team_date` (`sign_date`, `team_id`, `team_name`, `sign_count`) VALUES ('2021-01-25', 1, 'BARTeam', 1);
-INSERT INTO `signs_count_team_date` (`sign_date`, `team_id`, `team_name`, `sign_count`) VALUES ('2021-01-22', 2, 'ABC', 2);
-INSERT INTO `signs_count_team_date` (`sign_date`, `team_id`, `team_name`, `sign_count`) VALUES ('2021-01-24', 2, 'ABC', 4);
